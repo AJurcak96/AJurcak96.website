3bc77b371ee69f32458f23bf7910c159b7da4196
Aleksandar Jurčak <150478688+AJurcak96@users.noreply.github.com>
2024-03-28 11:19:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,3 +57,20 @@ audio.volume = 0.4;
         }
     });
 });
+
+// Show the button when scrolling down
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+}
+
+// Scroll to the top when the button is clicked
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
